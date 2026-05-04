3824c1458214454aac1ca23f9ad31b3692bbdd75
zero_guru <38360323+zerogtiger@users.noreply.github.com>
2026-05-04 18:01:50 -0400

    Moves implemented

--- src/cubie.rs
+++ src/cubie.rs
@@ -1,3 +1,9 @@
+use core::iter::Iterator;
+use std::default;
+use std::ops::Mul;
+use crate::moves::{Move::*, B_MOVE, D_MOVE, F_MOVE, L_MOVE, R_MOVE, U_MOVE};
+use crate::moves::Move;
+
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Corner {
@@ -111,3 +117,83 @@ pub struct Cubie {
     pub corner_orientation: [u8; 8],
     pub edge_orientation: [u8; 12],
 }
+
+impl Cubie {
+    pub fn new(
+        corner_permutation: [Corner; 8],
+        corner_orientation: [CornerOrientation; 8],
+        edge_permutation: [Edge; 12],
+        edge_orientation: [EdgeOrientation; 12]
+    ) -> Self {
+        Cubie {
+            corner_permutation: corner_permutation.map(|c| c as u8),
+            corner_orientation: corner_orientation.map(|c| c as u8),
+            edge_permutation: edge_permutation.map(|c| c as u8),
+            edge_orientation: edge_orientation.map(|c| c as u8)
+        }
+    }
+
+    pub fn apply_move(self, move_action: Move) -> Self {
+        let decomposed = match move_action {
+            U => U_MOVE,
+            U2 => U_MOVE * U_MOVE,
+            U3 => U_MOVE * U_MOVE * U_MOVE,
+            D => D_MOVE,
+            D2 => D_MOVE * D_MOVE,
+            D3 => D_MOVE * D_MOVE * D_MOVE,
+            F => F_MOVE,
+            F2 => F_MOVE * F_MOVE,
+            F3 => F_MOVE * F_MOVE * F_MOVE,
+            B => B_MOVE,
+            B2 => B_MOVE * B_MOVE,
+            B3 => B_MOVE * B_MOVE * B_MOVE,
+            L => L_MOVE,
+            L2 =>  L_MOVE * L_MOVE,
+            L3 =>  L_MOVE * L_MOVE * L_MOVE,
+            R => R_MOVE,
+            R2 => R_MOVE * R_MOVE,
+            R3 => R_MOVE * R_MOVE * R_MOVE,
+        };
+        self * decomposed
+    }
+
+    pub fn apply_moves(&self, moves: &[Move]) -> Self {
+        moves.iter().fold(*self, |acc, &m|acc.apply_move(m))
+    }
+}
+
+impl Default for Cubie {
+    fn default() -> Self {
+        Cubie {
+            corner_permutation: [0, 1, 2, 3, 4, 5, 6, 7],
+            edge_permutation: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+            corner_orientation: [0, 0, 0, 0, 0, 0, 0, 0],
+            edge_orientation: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        }
+    }
+}
+
+impl Mul for Cubie {
+    type Output = Cubie;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        // (A*B)(x).c=A(B(x).c).c
+        // (A*B)(x).o=A(B(x).c).o+B(x).o
+        let mut ret: Cubie = Default::default();
+        for i in 0..8 {
+            ret.corner_permutation[i] = self.corner_permutation[rhs.corner_permutation[i] as usize];
+            ret.corner_orientation[i] = (self.corner_orientation
+                [rhs.corner_permutation[i] as usize]
+                + rhs.corner_orientation[i])
+                % 3;
+        }
+        for i in 0..12 {
+            ret.edge_permutation[i] = self.edge_permutation[rhs.edge_permutation[i] as usize];
+            ret.edge_orientation[i] = (self.edge_orientation[rhs.edge_permutation[i] as usize]
+                + rhs.edge_orientation[i])
+                % 2;
+        }
+        ret
+    }
+}
+
---
+++ src/moves.rs
@@ -0,0 +1,78 @@
+use crate::cubie::Corner::*;
+use crate::cubie::CornerOrientation::*;
+use crate::cubie::Edge::*;
+use crate::cubie::EdgeOrientation::*;
+use crate::cubie::*;
+
+macro_rules! map_to_u8 {
+    ($($c:ident),*) => {
+        [$($c as u8), *]
+    };
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Move {
+    U,
+    U2,
+    U3,
+    D,
+    D2,
+    D3,
+    F,
+    F2,
+    F3,
+    B,
+    B2,
+    B3,
+    L,
+    L2,
+    L3,
+    R,
+    R2,
+    R3,
+}
+
+pub const U_MOVE: Cubie = Cubie {
+                                // URF, ULF, ULB, URB, DRF, DLF, DLB, DRB,
+    corner_permutation: map_to_u8![URB, URF, ULF, ULB, DRF, DLF, DLB, DRB],
+    corner_orientation: [0, 0, 0, 0, 0, 0, 0, 0],
+                              // UR, UF, UL, UB, DR, DF, DL, DB, FR, FL, BL, BR,
+    edge_permutation: map_to_u8![UB, UR, UF, UL, DR, DF, DL, DB, FR, FL, BL, BR],
+    edge_orientation: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+};
+
+pub const D_MOVE: Cubie = Cubie {
+    corner_permutation: map_to_u8![URF, ULF, ULB, URB, DLF, DLB, DRB, DRF],
+    corner_orientation: [0, 0, 0, 0, 0, 0, 0, 0],
+    edge_permutation: map_to_u8![UR, UF, UL, UB, DF, DL, DB, DR, FR, FL, BL, BR],
+    edge_orientation: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+};
+
+pub const R_MOVE: Cubie = Cubie {
+    corner_permutation: map_to_u8![DRF, ULF, ULB, URF, DRB, DLF, DLB, URB],
+    corner_orientation: map_to_u8![CCW, No, No, CW, CW, No, No, CCW],
+    edge_permutation: map_to_u8![FR, UF, UL, UB, BR, DF, DL, DB, DR, FL, BL, UR],
+    edge_orientation: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+};
+
+pub const L_MOVE: Cubie = Cubie {
+    corner_permutation: map_to_u8![URF, ULB, DLB, URB, DRF, ULF, DLF, DRB],
+    corner_orientation: map_to_u8![No, CW, CCW, No, No, CCW, CW, No],
+    edge_permutation: map_to_u8![UR, UF, BL, UB, DR, DF, FL, DB, FR, UL, DL, BR],
+    edge_orientation: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+};
+
+pub const F_MOVE: Cubie = Cubie {
+    corner_permutation: map_to_u8![ULF, DLF, ULB, URB, URF, DRF, DLB, DRB],
+    corner_orientation: map_to_u8![CW, CCW, No, No, CCW, CW, No, No],
+    edge_permutation: map_to_u8![UR, FL, UL, UB, DR, FR, DL, DB, UF, DF, BL, BR],
+    edge_orientation: [0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0],
+};
+
+pub const B_MOVE: Cubie = Cubie {
+    corner_permutation: map_to_u8![URF, ULF, URB, DRB, DRF, DLF, ULB, DLB],
+    corner_orientation: map_to_u8![No, No, CW, CCW, No, No, CCW, CW],
+    edge_permutation: map_to_u8![UR, UF, UL, BR, DR, DF, DL, BL, FR, FL, UB, DB],
+    edge_orientation: [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1],
+};
+
