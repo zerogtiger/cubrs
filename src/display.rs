168d502ab4cd6de899afbc8516765d2e62680f30
zero_guru <38360323+zerogtiger@users.noreply.github.com>
2026-05-04 18:03:18 -0400

    Cube displayable

---
+++ src/display.rs
@@ -0,0 +1,54 @@
+use crate::{facelet::{Color, Facelet}};
+
+pub struct CubeDisplay {
+    colors: [char; 6], // Color of U, R, F, D, L, B
+}
+
+impl CubeDisplay {
+    pub fn new(colors: [char; 6]) -> Self {
+        CubeDisplay { colors }
+    }
+
+    fn realize_color(&self, color: Color) -> char {
+        self.colors[match color {
+            Color::U(_) => 0,
+            Color::R(_) => 1,
+            Color::F(_) => 2,
+            Color::D(_) => 3,
+            Color::L(_) => 4,
+            Color::B(_) => 5,
+        }]
+    }
+
+    pub fn display_cube(&self, facelet: &Facelet) {
+        for i in 0..3 {
+            print!(". . . ");
+            for j in 0..3 {
+                print!("{} ", self.realize_color(facelet.get_face(&Color::U(i * 3 + j))));
+            }
+            println!();
+        }
+        for i in 0..3 {
+            for j in 0..3 {
+                print!("{} ", self.realize_color(facelet.get_face(&Color::L(i * 3 + j))));
+            }
+            for j in 0..3 {
+                print!("{} ", self.realize_color(facelet.get_face(&Color::F(i * 3 + j))));
+            }
+            for j in 0..3 {
+                print!("{} ", self.realize_color(facelet.get_face(&Color::R(i * 3 + j))));
+            }
+            for j in 0..3 {
+                print!("{} ", self.realize_color(facelet.get_face(&Color::B(i * 3 + j))));
+            }
+            println!();
+        }
+        for i in 0..3 {
+            print!(". . . ");
+            for j in 0..3 {
+                print!("{} ", self.realize_color(facelet.get_face(&Color::D(i * 3 + j))));
+            }
+            println!();
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 pub mod cubie;
+pub mod moves;
+pub mod display;
+pub mod facelet;
+
+use core::default::Default;
+use moves::Move::*;
+
+use crate::{cubie::Cubie, display::CubeDisplay};
 
 fn main() {
-    println!("Hello, world!");
+    let mut cube: Cubie = Default::default();
+    let moves = [U, D, L3, B2];
+    cube = cube.apply_moves(&moves);
+    let cube_display = CubeDisplay::new(['W', 'R', 'G', 'Y', 'O', 'B']);
+    cube_display.display_cube(&facelet::Facelet::from_cubie(&cube));
 }
-
-
