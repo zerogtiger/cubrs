1e8aa4824b40d106ca5b823487de46f438c82c94
zero_guru <38360323+zerogtiger@users.noreply.github.com>
2026-05-06 09:42:34 -0400

    nCk function added checks for negative k

--- src/cubie.rs
+++ src/cubie.rs
@@ -9,9 +9,12 @@ pub const FACTORIAL: [u32; 13] = [
     1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600,
 ];
 
-pub fn binomial_coefficient(n: u8, k: u8) -> u32 {
+pub fn binomial_coefficient(n: i8, k: i8) -> u32 {
     assert!(n <= 12);
     assert!(k <= 12);
+    if k < 0 {
+        return 0;
+    }
     match k > n {
         true => 0,
         false => FACTORIAL[n as usize] / (FACTORIAL[k as usize] * FACTORIAL[(n - k) as usize]),
@@ -240,7 +243,7 @@ impl Cubie {
                         -1 => 0,
                         _ => {
                             // max value is 11C3 = 462 << u16MAX
-                            binomial_coefficient(idx as u8, cnt as u8) as u16
+                            binomial_coefficient(idx as i8, cnt as i8) as u16
                         }
                     }
                 }
