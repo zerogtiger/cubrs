8ec4355791bcb3c06926247dbefed60058ec4bb3
zero_guru <38360323+zerogtiger@users.noreply.github.com>
2026-06-28 23:56:05 -0400

    Superflip benchmark added

--- benches/solver_benchmark.rs
+++ benches/solver_benchmark.rs
@@ -1,19 +1,10 @@
 use criterion::{Criterion, criterion_group, criterion_main};
-use cube_timer::{cubie::*, solver::Solver};
+use cube_timer::{cubie::*, moves::Move, solver::Solver};
 use rand::{RngExt, SeedableRng, rngs::StdRng};
 use rand_chacha::ChaCha8Rng;
-use std::{hint::black_box, time::Duration};
+use std::{default, hint::black_box, time::Duration};
 
-// fn fibonacci(n: u64) -> u64 {
-//     match n {
-//         0 => 1,
-//         1 => 1,
-//         n => fibonacci(n-1) + fibonacci(n-2),
-//     }
-// }
-
-pub const MOVE_LIMIT: u8 = 20;
-pub const COUNT: usize = 100;
+pub const MOVE_LIMIT: u8 = 21;
 
 fn solve_cube(
     solver: &Solver,
@@ -30,7 +21,8 @@ fn solve_cube(
     let _ = solver.solve(&cube, MOVE_LIMIT);
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn multi_solve_benchmark(c: &mut Criterion) {
+    const COUNT: usize = 1000;
     const SEED: u64 = 1;
 
     let mut rng = ChaCha8Rng::seed_from_u64(SEED);
@@ -70,7 +62,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     }
 
     let mut group = c.benchmark_group("cube solver");
-    group.sample_size(16);
+    group.sample_size(20);
     group.measurement_time(Duration::from_secs(60));
     group.noise_threshold(0.05);
     group.bench_function("100 Random states", |b| {
@@ -83,5 +75,39 @@ fn criterion_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn single_superflip(c: &mut Criterion) {
+    let superflip_move_list =
+        Move::move_list_from_str("U R2 F B R B2 R U2 L B2 R U' D' R2 F R' L B2 U2 F2").unwrap();
+    let mut cube = Cubie::default();
+    cube = cube.apply_moves(&superflip_move_list);
+
+    let solver = Solver::new();
+
+    let solution = solver.solve(&cube, MOVE_LIMIT).unwrap();
+    let solved_cube = cube.apply_moves(&solution);
+    assert_eq!(solved_cube.corner_permutation_coord(), 0);
+    assert_eq!(solved_cube.corner_orientation_coord(), 0);
+    assert_eq!(solved_cube.edge_permutation_coord(), 0);
+    assert_eq!(solved_cube.edge_orientation_coord(), 0);
+
+    let mut group = c.benchmark_group("cube solver");
+    group.sample_size(20);
+    group.measurement_time(Duration::from_secs(60));
+    group.noise_threshold(0.05);
+    group.bench_function("superflip", |b| {
+        b.iter(|| {
+            solve_cube(
+                black_box(&solver),
+                cube.corner_permutation_coord(),
+                cube.corner_orientation_coord(),
+                cube.edge_permutation_coord(),
+                cube.edge_orientation_coord(),
+            );
+        })
+    });
+    group.finish();
+}
+
+criterion_group!(benches, multi_solve_benchmark);
+// criterion_group!(benches, single_superflip);
 criterion_main!(benches);
